806cac088b636917964c866c15ec219d2aa32221
Ofer Goren <ogoren@multivutech.com>
2025-11-10 22:20:27 +0200

    fix error

--- earnings/earnings.py
+++ earnings/earnings.py
@@ -17,12 +17,16 @@ if sys.version_info < (3, 10):
             def packages_distributions():
                 pkg_to_dist = {}
                 for dist in importlib_metadata.distributions():
-                    if dist.files:
-                        for file in dist.files:
-                            pkg = str(file).split('/')[0]
-                            if pkg not in pkg_to_dist:
-                                pkg_to_dist[pkg] = []
-                            pkg_to_dist[pkg].append(dist.name)
+                    try:
+                        dist_name = dist.metadata.get('Name') or getattr(dist, '_name', 'unknown')
+                        if dist.files:
+                            for file in dist.files:
+                                pkg = str(file).split('/')[0]
+                                if pkg not in pkg_to_dist:
+                                    pkg_to_dist[pkg] = []
+                                pkg_to_dist[pkg].append(dist_name)
+                    except Exception:
+                        continue
                 return pkg_to_dist
             importlib_metadata.packages_distributions = packages_distributions
     except Exception:
